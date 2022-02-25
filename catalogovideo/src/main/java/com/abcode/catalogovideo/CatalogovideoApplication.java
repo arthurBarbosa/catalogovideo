107f0b67081e7b18c168eb9be9cab03058601b3a
Arthur Barbosa <arthurbruno03@gmail.com>
2022-02-25 20:43:08 -0300

    projeto criado

---
+++ catalogovideo/src/main/java/com/abcode/catalogovideo/CatalogovideoApplication.java
@@ -0,0 +1,13 @@
+package com.abcode.catalogovideo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogovideoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogovideoApplication.class, args);
+	}
+
+}
---
+++ catalogovideo/src/test/java/com/abcode/catalogovideo/CatalogovideoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abcode.catalogovideo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogovideoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
