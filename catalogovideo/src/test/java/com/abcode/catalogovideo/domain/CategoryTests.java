5d4b6d6184f43048c7ef7f11ac10fcbec6f29dea
Arthur Barbosa <arthurbruno03@gmail.com>
2022-02-25 21:58:09 -0300

    create unit test createCategoryWithName domain category

--- catalogovideo/src/test/java/com/abcode/catalogovideo/domain/CategoryTests.java
+++ catalogovideo/src/test/java/com/abcode/catalogovideo/domain/CategoryTests.java
@@ -1,16 +1,25 @@
 package com.abcode.catalogovideo.domain;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class CategoryTests {
 
     @Test
-    public void instantiate(){
-        final Category entity = new Category();
+    public void createCategoryWithNames() throws Exception {
+        final Category entity = new Category(
+                "Name 1",
+                "description 2"
+        );
+
+        assertNotNull(entity);
+        Assertions.assertEquals(entity.getName(), "Name 1");
     }
 }
